d099ed9665a1ad41512ca571c1d2795562c1e7ae
Eduardo Fares <edufarest@gmail.com>
2019-10-16 15:08:33 -0400

    Calculate c matrices

--- framething.py
+++ framething.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3
 
 import math
+from copy import deepcopy
 # Generate main
 
 image = [[0 for item in range(8)] for j in range(8)]
@@ -10,22 +11,32 @@ prewittY = [[1, 1, 1], [0, 0, 0], [-1, -1, -1]]
 sobelX = [[-1, 0, 1], [-2, 0, 2], [-1, 0, -1]]
 sobelY = [[-1, -2, -1], [0,0,0], [1, 2, 1]]
 
+
+checker = []
+
+for i in range(10):
+    checker.append([0,0,0,0,0,0,0,0,0,0,40,40,40,40,40,40,40,40,40,40])
+
+for i in range(10):
+    checker.append([40,40,40,40,40,40,40,40,40,40,0,0,0,0,0,0,0,0,0,0])
+
+
 for x in range(0, 8):
     for y in range(0, 8):
         image[x][y] = abs(x - y)
 
 
 
-def printImage(image):
-    for x in range(8):
-        for y in range(8):
-            print('%.3f'%(image[x][y]), end=', ')
+def printImage(image, size=8):
+    for x in range(size):
+        for y in range(size):
+            print('%.1f'%(image[x][y]), end=', ')
         print("")
 
-def applyFilter(image, fil):
-    result = image.copy()
-    for x in range(1, 7):
-        for y in range(1, 7):
+def applyFilter(image, fil, size=8):
+    result = deepcopy(image)
+    for x in range(1, size-1):
+        for y in range(1, size-1):
 
             frameVal = 0
 
@@ -34,33 +45,87 @@ def applyFilter(image, fil):
 
                     frameVal += image[x+i][y+j] * fil[i+1][j+1]
 
-            result[x][y] = float('%.3f'%(frameVal/9))
+            result[x][y] = float('%.1f'%(frameVal/9))
 
     return result
+
+def applyFunction(img1, img2, fun, size=8):
+    result = [[0 for item in range(size)] for j in range(size)]
+    for x in range(size):
+        for y in range(size):
+            result[x][y] = fun(img1[x][y], img2[x][y])
+
+    return result
+
+def magnitude(imgx, imgy, size=8):
+    return  applyFunction(imgx, imgy, lambda x, y : pow(pow(x, 2) + pow(y, 2)  , 0.5), size)
+ 
+
+def direction(imgx, imgy, size=8):
+    return applyFunction(imgx, imgy, lambda x, y : math.atan2(y, x), size)
+
+
+
+#printImage(checker, 20)
+cx = applyFilter(checker, prewittX, 20)
+cy = applyFilter(checker, prewittY, 20)
+
+c = magnitude(cx, cy, 20)
+
+
+def cmatrix(imgx, imgy, size=20, nbhd=7):
+
+    cmatrix = []
+
+    for i in range(0, size-nbhd+1):
+        for j in range(0, size-nbhd+1):
+
+            cx = 0
+            cy = 0
+            cxy= 0
+
+            for x in range(i, i+nbhd):
+                for y in range(j, j+nbhd):
+                    cx += imgx[x][y]*imgx[x][y]
+                    cy += imgy[x][y]*imgy[x][y]
+                    cxy+= imgx[x][y]*imgy[x][y]
+
+            cx = int(cx)
+            cy = int(cy)
+            cxy= int(cxy)
+
+            cmatrix.append([[cx, cxy], [cxy, cy]])
+
+    return cmatrix
+
+
+def printCMatrix(cmatrix, nbhd=7, size=20):
+    
+    i = 0
+    while i < pow(size-nbhd+1, 2):
+        for j in range(size-nbhd+1):
+            print(cmatrix[i+j][0], end=";")
+        print(" ")
+        for j in range(size-nbhd+1):
+            print(cmatrix[i+j][1], end=";")
+        print("\n")
+        i += size-nbhd+1
+
+
+cmatrix = cmatrix(cx, cy)
+printCMatrix(cmatrix)
+
+
 
 #printImage(image)
 
 gx = applyFilter(image, prewittX)
 gy = applyFilter(image, prewittY)
 
-def applyFunction(img1, img2, fun):
-    result = [[0 for item in range(8)] for j in range(8)]
-    for x in range(8):
-        for y in range(8):
-            result[x][y] = fun(img1[x][y], img2[x][y])
 
-    return result
+g = applyFunction(gx, gy, lambda x, y : pow(pow(x, 2) + pow(y, 2)  , 0.5))
 
-def magnitude(imgx, imgy):
-    return  applyFunction(imgx, imgy, lambda x, y : pow(pow(x, 2) + pow(y, 2)  , 0.5))
- 
-
-def direction(imgx, imgy):
-    return applyFunction(imgx, imgy, lambda x, y : math.atan2(y, x))
-
-#g = applyFunction(gx, gy, lambda x, y : pow(pow(x, 2) + pow(y, 2)  , 0.5))
-
-#gDir = applyFunction(gx, gy, lambda x, y : math.atan2(y, x))
+gDir = applyFunction(gx, gy, lambda x, y : math.atan2(y, x))
 
 #printImage(gDir)
 
@@ -69,4 +134,3 @@ sy = applyFilter(image, sobelY)
 
 s = magnitude(sx, sy)
 dirS = direction(sx, sy)
-printImage(dirS)
